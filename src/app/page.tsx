685119fae25adc95e9fcf24c3f6b5c2568e94104
pxpi1ot <pxpilot@outlook.com>
2025-09-17 20:58:26 +0800

    添加Prisma和TRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-   <div className="">
-    <Button>Click me</Button>
-   </div> 
-  );
+	const trpc = useTRPC();
+	const { data } = useQuery(trpc.hello.queryOptions({ text: "Hello!" }));
+
+	return (
+		<div className="">
+		  {JSON.stringify(data)}
+		</div>
+	);
 }
